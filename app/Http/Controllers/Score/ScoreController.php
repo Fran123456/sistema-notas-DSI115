93c53e8b21775cd32cb26ceab9a06ba6ebef4797
Frank <navasfran98@gmail.com>
2020-09-23 19:21:08 -0600

    notas 1

--- app/Http/Controllers/Score/ScoreController.php
+++ app/Http/Controllers/Score/ScoreController.php
@@ -5,10 +5,23 @@ namespace App\Http\Controllers\Score;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ScoreStudent;
-
+use App\SchoolYear;
+use App\Degree;
 class ScoreController extends Controller
 {
-    public SendTypes($period){
+    public function  getScoresTypeByStudent(Request $request){
+      //request => year , degree
+      $year = SchoolYear::find($request->year);
+      $degree = SchoolYear::find($request->degree);
+      $look = $request->look;
 
+      $types = array();
+      if(isset($request->student)){ //existe
+
+      }else{ //no existe
+
+      }
+      return view('score.score.scoreStudent', compact('types','look'));
     }
+
 }
--- app/ScoreStudent.php
+++ app/ScoreStudent.php
@@ -11,4 +11,35 @@ class ScoreStudent extends Model
     'id','score_type_id','student_id','school_period_id','school_year_id','degree_id','score',
     'created_at','updated_at','subject_id'
   ];
+
+  public function score_types()
+  {
+      return $this->belongsTo('App\ScoreType','score_type_id');
+  }
+
+  public function student()
+  {
+      return $this->belongsTo('App\Student','student_id');
+  }
+
+  public function school_period()
+  {
+      return $this->belongsTo('App\SchoolPeriod','school_period_id');
+  }
+
+  public function school_year()
+  {
+      return $this->belongsTo('App\SchoolYear','school_year_id');
+  }
+
+  public function degree()
+  {
+      return $this->belongsTo('App\Degree','degree_id');
+  }
+
+  public function subject()
+  {
+      return $this->belongsTo('App\Subject','subject_id');
+  }
+
 }
