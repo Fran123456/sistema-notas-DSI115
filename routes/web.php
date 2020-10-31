02a6d6713de8d7e886b972c29feb789e69079b51
YOVANIHA <yovaniha96@gmail.com>
2020-10-31 13:52:31 -0600

    mostrar materias por grado

--- routes/web.php
+++ routes/web.php
@@ -108,6 +108,7 @@ Route::post('grades-teacher/percentage/save','Score\ScoreTypeController@scoreTyp
 Route::get('grades-teacher/percentage/{grade}/{teacher}/{subject}/{period}','Teacher\TeacherController@scorePercentage')->name('scorePercentage');
 Route::get('teacher/{idteacher}/degree/{iddegree}/students/','Teacher\TeacherController@showStudentsDegreeTeacher')->name('showStudentsDegreeTeacher');
 Route::get('types/sendtypes', 'Score\ScoreTypeController@SendTypes')->name('SendTypes');
+Route::get('teacher/{idteacher}/degree/{iddegree}/subjects/','Teacher\TeacherController@showSubjectsByDegree')->name('showSubjectsByDegree');
 
 /*score*/
 Route::get('scores/student', 'Score\ScoreController@getScoresTypeByStudent')->name('getScoresTypeByStudent');
