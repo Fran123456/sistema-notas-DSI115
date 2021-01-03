40dd9c4f311ca9f63a75dc79a28ce89dd60d578f
Miguel Luis Perez <50308255+mikezxcv@users.noreply.github.com>
2021-01-02 23:21:53 -0600

    fixes menores eeder asistencia

--- database/seeds/AttendanceStudentSeeder.php
+++ database/seeds/AttendanceStudentSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\AttendanceStudent;
 use App\StudentHistory;
+use Carbon\Carbon;
 
 class AttendanceStudentSeeder extends Seeder
 {
@@ -18,13 +19,14 @@ class AttendanceStudentSeeder extends Seeder
         array_push($dates,'2020-09-13','2020-09-14','2020-09-15');
         foreach($dates as $date){
             foreach($studentHistory as $student){
-            $attendanceStudent= new AttendanceStudent();            
-            $attendanceStudent->attendance_date=$date;
+            $attendanceStudent= new AttendanceStudent();
+            $randomDate = Carbon::today()->subDays(rand(0, 365));
+            $attendanceStudent->attendance_date=$randomDate;
             $attendanceStudent->student_history_id=$student->id;
             $random=random_int(0,2); $random2=random_int(1,3);
             $attendanceStudent->active=$random;
-            //$attendanceStudent->period_id=$random2;
-            $attendanceStudent->period_id=1;
+            $attendanceStudent->period_id=$random2;
+            //$attendanceStudent->period_id=1;
             $attendanceStudent->save();
         }
         }
